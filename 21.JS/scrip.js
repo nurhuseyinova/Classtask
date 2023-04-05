571d5af41027aeb3cda4ac44992df9060992d160
Nurlana <126771913+nurhuseyinova@users.noreply.github.com>
2023-04-05 07:35:56 -0700

    Add files via upload

---
+++ 21.JS/scrip.js
@@ -0,0 +1,41 @@
+console.log(12*8)
+
+let x=10/2;
+console.log(x)
+
+let num1=20;
+let num2=17;
+console.log(num1+num2)
+
+let firstName="Nurlana";
+let surname="Huseyinova";
+let year=2004;
+let info=(firstName+surname+year);
+console.log(info)
+
+console.log(17%12)
+
+var cityName="Shamkir";
+cityName="Gence";
+console.log(cityName)
+
+let yourName={
+    myName:"Nurlana",
+    mySurname:"Huseyinova"
+}
+let yourName1={...yourName}
+console.log(yourName1)
+
+let yourCity={
+    yourCity:"Gence",
+    yourYear:2003
+
+}
+let yourCity2={
+    ...yourCity,
+    yourCity:"Shamkir",
+    yourYear:2004
+}
+console.log(yourCity)
+
+
