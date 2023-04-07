71ee644c1ff573d54241ba98f04c1b9221dc48c9
Nurlana <126771913+nurhuseyinova@users.noreply.github.com>
2023-04-07 06:00:51 -0700

    Add files via upload

---
+++ 23.JS-do,while/main.js
@@ -0,0 +1,40 @@
+
+let i=0
+while(i<10){
+i++
+    console.log(`${i}x${i}=${i*i}`);
+
+}
+
+let n=0
+while(n<10){
+n++
+    console.log(`${n}x${n} ${n**2}  ${n**3}`);
+
+}
+
+for(let h=0; h<=10; h++){
+    console.log(`${h}x${h}=${h*h}`);
+
+}
+
+
+let myName="Nurlana"
+for( let i=0; i<myName.length; i++){
+    console.log(myName);
+}
+
+for(let i=0; i<=8; i++){
+    if(i %2 == 0 )
+    console.log(i);
+}
+
+for(let i=0; i<8; i++){
+    if(i%2==0){
+        console.log(`${i}  even`)}
+
+    else{
+         console.log(`${i}  odd`)}
+   
+}
+
