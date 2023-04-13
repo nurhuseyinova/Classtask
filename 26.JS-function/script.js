c3b9a544e86d516b6b877f41321f7741dd267830
Nurlana Huseyinova <126771913+nurhuseyinova@users.noreply.github.com>
2023-04-13 07:07:32 -0700

    Add files via upload

---
+++ 26.JS-function/script.js
@@ -0,0 +1,26 @@
+//1
+// function sayHi(num) {
+//     let x=num%10
+//     let y=num%100
+//     let z=num%1000
+// console.log(x);
+// console.log(y);
+// console.log(z);
+     
+   
+// }
+// sayHi(155)
+
+//2
+// function sayH(num) {
+//     let x=num%10
+//     let y=(num%100-x)/10
+//     let z=((num-num%100)/100)
+    
+// console.log(x);
+// console.log(y);
+// console.log(z);
+     
+   
+// }
+// sayH(155)
