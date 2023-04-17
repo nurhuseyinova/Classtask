270df60fd3ee1f600ff298ff8566454ce43f147d
Nurlana Huseyinova <126771913+nurhuseyinova@users.noreply.github.com>
2023-04-17 07:27:28 -0700

    Add files via upload

---
+++ 29.JS-arrays-string-methods/methods.js
@@ -0,0 +1,66 @@
+// /////1+
+// const arr1 = ['guba', 'mingecevir', 'baku', 'gazag', 'gence', 'shirvan'];
+// let result=arr1.map((el)=>el[0].toUpperCase()+el.slice(1,-1)+el.at(-1).toUpperCase());
+// console.log(result);
+
+//////2
+// let text = 'We are MERN-Stack developers';
+////1+
+// console.log(text.replace("MERN-Stack","Front-end"));
+
+////2+
+// let result1=text.toLowerCase();
+// console.log((result1.replaceAll("e","a")));
+
+////3+
+// console.log(text.concat("in Microsoft!"));
+
+////4+
+// let newArr=[]
+// text.split("").map((el,index)=>el.toLowerCase()==='r' ? newArr.push(index):newArr)
+// console.log(newArr);
+
+////5+
+// let result2=text.toLowerCase();
+// console.log(result2.indexOf('r',5));
+
+////6
+
+
+
+
+//////3+
+// const arr3 = [
+//         {
+//             region: 'Yasamal',
+//             area: 170,
+//         },
+    
+//         {
+//             region: 'Nizami',
+//             area: 20,
+//         },
+    
+//         {
+//             region: 'Nesimi',
+//             area: 10,
+//         },
+    
+//         {
+//             region: 'Sebail',
+//             area: 30,
+//         },
+//         {
+//             region: 'Xetai',
+//             area: 30,
+//         },
+    
+//     ]
+//     console.log( arr3.reduce((acc, curr)=>curr.region.length==6 ? acc+curr.area:acc,0));
+
+/////4+
+// const arr4 = [8, 44, 14, 2, 23, 1, 22, 34, 9, 0, 6];
+// console.log(arr4.sort((a,b)=>a-b));
+
+    
+ 
