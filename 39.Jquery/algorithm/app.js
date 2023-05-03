786e80d5cb17b7c7faf3c423091446ad296e8214
Nurlana Huseyinova <126771913+nurhuseyinova@users.noreply.github.com>
2023-05-03 08:06:58 -0700

    Add files via upload

---
+++ 39.Jquery/algorithm/app.js
@@ -0,0 +1,28 @@
+
+//1+
+function capitalizeFirst(str) {
+return str.toLowerCase().split(' ').map(x=>x[0].toUpperCase()+x.slice(1)).join(' ');
+}
+console.log(capitalizeFirst("This is a title"));
+console.log(capitalizeFirst("capitalize every word"));
+
+//2
+function encoded(str) {
+    let arr=str.split``
+    eturn arr.map(el=>el.length>4?str.fill("*"))n hnhnjhhhnjhnjhnjhnjhfvrthgtgggbtfgg,n,kn.join``q2aw
+
+
+    
+
+    
+
+
+}
+console.log(encoded("The code is fourty"));
+console.log(encoded("Two plus three is five"));
+console.log(encoded("aaaa aaaaa 1234 123456"));
+
+
+wdc
+
+v v ...capitalizeFirst.s;;; 90j\z
---
+++ 39.Jquery/script.js
@@ -0,0 +1,30 @@
+
+// const colors=['red','green','blue','yellow','orange','brown']
+
+// //////1
+// $(".change").on("click",function(){
+//     $('.title').each(function() {
+//         $(this).css('color', colors[Math.floor(Math.random() * colors.length)]);
+//     }) })
+// /////2
+// $(".hide").on("click",function(){
+//     $(".title").hide()
+// })
+// $(".show").on("click",function(){
+//     $(".title").show()
+// })
+// ////3
+// $.each(colors, function( index, value ) {
+//     console.log( value );
+//   });
+
+// ////4
+// const numbers =[4,2,false,null,'false',NaN,undefined,8,true,'9']
+// let sum=0
+// $.map( numbers, function( val, i ) {
+//     if(val){
+
+//     sum+=i;
+//     }
+//   });
+//   console.log(sum)
