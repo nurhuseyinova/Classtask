4e0f89b900d037062d497c83c3e46767f70a83c5
Nurlana Huseyinova <80hd0r0@code.edu.az>
2023-05-14 17:45:38 +0400

    feat: crud-form,sort,search

--- 43.Crud/app.js
+++ 43.Crud/app.js
@@ -1,7 +1,7 @@
-let API_URL = `http://localhost:8000/users`;
+let API_URL = `http://localhost:8080/users`;
 
 let form = document.querySelector("#userForm");
-let firsName = document.querySelector("#firsName");
+let firstName = document.querySelector("#firstName");
 let lastName = document.querySelector("#lastName");
 let emailInput = document.querySelector("#email");
 let numberInput = document.querySelector("#number");
@@ -31,10 +31,16 @@ async function getData() {
     <td>${el.firstName}</td>
     <td>${el.lastName}</td>
     <td>${el.email}</td>
-    <td>${el.number}</td>
-    <td><a href="#" id="${el.id} "onclick=editBtn("${el.id}") class="btn btn-success">Edit</a>
-    <a href="" id="${el.id}" onclick=deleteBtn("${el.id}") class="btn btn-danger">Delete</a>
-    <a href="" id="${el.id}" onclick=detailBtn("${el.id}") class="btn btn-primary">Detail</a></td>
+    <td>${el.number.slice(-4).padStart(el.number.length, "*")}</td>
+    <td><a href="#" id="${el.id} "onclick=editBtn("${
+      el.id
+    }") class="btn btn-success">Edit</a>
+    <a href="" id="${el.id}" onclick=deleteBtn("${
+      el.id
+    }") class="btn btn-danger">Delete</a>
+    <a href="" id="${el.id}" onclick=detailBtn("${
+      el.id
+    }") class="btn btn-primary">Detail</a></td>
    
     </tr>
     `;
@@ -44,79 +50,85 @@ async function getData() {
 }
 getData();
 
+function showAlert(message, className) {
+  alertBtn.innerHTML = message;
+  alertBtn.className = `alert alert-${className}`;
+  alertBtn.removeAttribute("hidden");
+  setTimeout(() => {
+    alertBtn.setAttribute('hidden','')
+  }, 2000);
+}
+
 function deleteBtn(id, btn) {
+  showAlert("DeletedUser", "danger");
   axios.delete(`${API_URL}/${id}`);
   btn.closest("tr").remove();
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  const date = new Date();
   let obj = {
-    firstName: firsName.value,
+    firstName: firstName.value,
     lastName: lastName.value,
     email: emailInput.value,
-    number:numberInput.value.slice(-4).padStart(numberInput.value.length,"*")
-    
+    number: numberInput.value,
+    date: date.toLocaleDateString,
+  };
+  if (
+    firstName.value &&
+    lastName.value &&
+    emailInput.value &&
+    numberInput.value
+  ) {
+    if (!editStatus) {
+      axios.post(`${API_URL}`, obj);
+      showAlert("Add User succesfully!", `info`);
+    } else {
+      axios.patch(`${API_URL}/${editId}`, obj);
+      editStatus = false;
+      showAlert("Edit User succesfully!", `success`);
+    }
   }
-  if(firsName.value&&lastName.value&&emailInput.value&&numberInput.value){
-
-  if (!editStatus) {
-    axios.post(`${API_URL}`, obj);
-
-
-  } else {
-    axios.patch(`${API_URL}/${editId}`, obj);
-    editStatus = false;
-
-  }}
 });
 
 function editBtn(id) {
   editId = id;
   editStatus = true;
   let edit = arr.filter((el) => el.id == editId);
-
   edit.forEach((item) => {
-    firsName.value = item.firstName;
+    firstName.value = item.firstName;
     lastName.value = item.lastName;
     emailInput.value = item.email;
   });
-
-  submitBtn.innerText="Edit"
+  submitBtn.innerText = "Edit";
 }
-
-
-
-
-
-search.addEventListener("input",function(e){
-  copyData=arr
-  copyData=copyData.filter((item=>item.firstName.toLowerCase().includes(search.value.toLowerCase())))
-  getData()
-})
-
-
+search.addEventListener("input", function (e) {
+  copyData = arr;
+  copyData = copyData.filter((item) =>
+    item.firstName.toLowerCase().includes(search.value.toLowerCase())
+  );
+  getData();
+});
 arrowUp.style.display = "none";
 arrowDown.style.display = "none";
-// let asc=true
-// firstNameClick.addEventListener("click",function(){
-//   let sortedName;
-//   asc=!asc
-//   if(!asc){
-//     copyData=arr
-//     copyData.sort((a,b)=>b.firsName.localeCompare(a.firsName))
-//     console.log(copyData);
-//   arrowUp.style.display="none"
-//   arrowDown.style.display="inline"
-//   getData()
-
-//   }
-
-//   else{
-//     copyData.sort((a,b)=>a.firsName.localeCompare(b.firsName))
-//     arrowUp.style.display="inline"
-//   arrowDown.style.display="none"
-//   getData()
-//   }
-
-// })
+let asc = true;
+firstNameClick.addEventListener("click", function () {
+  asc = !asc;
+  if (!asc) {
+    copyData = arr;
+    copyData.sort((a, b) => b.firstName.localeCompare(a.firstName));
+    arrowUp.style.display = "none";
+    arrowDown.style.display = "inline";
+    getData();
+  } else {
+    copyData.sort((a, b) => a.firstName.localeCompare(b.firstName));
+    arrowUp.style.display = "inline";
+    arrowDown.style.display = "none";
+    getData();
+  }
+});
+async function getUserDetail(userId, username, userDate) {
+  await axios.get(`${API_URL}/${userId}`);
+  showAlert(`User: ${username}  created-${userDate}`, `info`);
+}
