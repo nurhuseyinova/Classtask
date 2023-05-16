b0985e2a0487836828d20c22233b121c75b5bb68
Nurlana Huseyinova <80hd0r0@code.edu.az>
2023-05-16 12:58:15 +0400

    feat: header-toggle

--- 46.header/app.js
+++ 46.header/app.js
@@ -1,12 +1,12 @@
 let bar = document.querySelector(".menu");
 let header = document.querySelector(".header");
-let xmark=document.querySelector(".fa-xmark")
+
 
 bar.addEventListener("click", function () {
   header.classList.toggle("show");
-  xmark.classList="white"
- if(bar.classList = "fa-bars"){
-    (bar.classList = "fa-solid fa-xmark")}
-    if(bar.classList = "fa-solid fa-xmark")
-    { (bar.classList = "fa-solid fa-bars")}
+  
+//  if(bar.classList = "fa-bars"){
+//     (bar.classList = "fa-solid fa-xmark")}
+//     if(bar.classList = "fa-solid fa-xmark")
+//     { (bar.classList = "fa-solid fa-bars")}
 });
