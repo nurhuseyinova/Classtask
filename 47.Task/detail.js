eb758b11212db94ae50339de6fdf98c420fff715
Nurlana Huseyinova <80hd0r0@code.edu.az>
2023-05-18 23:55:24 +0400

    feat: update api

--- 47.Task/detail.js
+++ 47.Task/detail.js
@@ -1,38 +1,40 @@
 let API_URL = `https://restcountries.com/v3.1/name`;
 let names = new URLSearchParams(window.location.search).get("name");
 let detailEL = document.querySelector(".detail");
+console.log(names);
 
 async function detailData() {
   let res = await axios(`${API_URL}/${names}`);
   let data = await res.data;
+  console.log(data);
   detailEL.innerHTML = `
         <div class="col col-12 col-md-6 col-sm-12 image">
-        <img src="${data.flags.svg}" alt="">
+        <img src="${data[0].flags.svg}" alt="">
     </div>
     <div class="col col-12 col-md-6 col-sm-12 information">
       <div class="info">
         <div class="information1">
-            <h1>${data.name}</h1>
-            <p>Nativa Name: ${data.nativeName} </p>
-            <p>Population: ${data.population}</p>
-            <p>Region: ${data.region}</p>
-            <p>Sub Region: ${data.subregion}</p>
-            <p>Capital: ${data.capital}</p>
+            <h1>${data[0].name.common}</h1>
+            <p>Population:${data[0].population} </p>
+            <p>Region: ${data[0].region}</p>
+            <p>Sub Region:${data[0].subregion} </p>
+           <p>Capital: ${data[0].capital}</p>
            </div>
-           <div class="information2">
-            <p>Top Level Domain: ${data.topLevelDomain}</p>
-            <p>Currencies: ${data.currencies.name}</p>
-            <p>Languages: ${data.languages.languages[0].name},${element.languages.languages[1].name},${element.languages.languages[2].name}</p>
-           </div>
-      </div>
-       <div class="information2 my-4">
-        <p>Border countries
-            <button>${data.borders.borders[0]}</button> 
-            <button>${data.borders.borders[1]}</button> 
-            
-        </p>
-       </div>
-    </div>
-        `
-}
-detailData();
+                   <div class="information2">
+                   <p>Top Level Domain: ${data[0].tld[0]}</p>
+                   <p>Domain: ${data[0].flag}</p>
+                   </div>
+                   </div>
+                   <div class="information2 my-4">
+                   
+                   <p>Border countries
+                   <button class="btn btn-info">${data[0].borders[0]}</button> 
+                   <button class="btn btn-info">${data[0].borders[1]}</button> 
+                   <button class="btn btn-info">${data[0].borders[2]}</button> 
+                   </p>
+                   </div>
+                   </div>
+                   `;
+                  }
+                  detailData();
+               
